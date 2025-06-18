4ca4bf2e0ac8443438aa48149b69e18e782774da
JensHinger <hinger.jens@gmail.com>
2025-06-18 09:40:43 +0200

    added correct markdown formatting and styling to chat messages

--- chat-frontend/src/app/Components/Chat/ChatMessage.tsx
+++ chat-frontend/src/app/Components/Chat/ChatMessage.tsx
@@ -1,16 +1,17 @@
 import IMessage from "@/Models/IMessage"
 import styles from "./chatMessage.module.css"
-
+import {marked} from 'marked';
 
 export default function ChatMessage(
     {messageProps}:{messageProps: IMessage}
 ){
     // Style should be based on role either left or right bound and color should differ
+    const parsedMessage = marked(messageProps.text)
 
     var messageStyling = messageProps.author == "user"? styles.userMessage : styles.agentMessage
     return (
-        <div className={`${messageStyling} ${styles.message}`}>
-            {messageProps.text}
-        </div>
+        <div 
+            className={`${messageStyling} ${styles.message}`} 
+            dangerouslySetInnerHTML={{__html: parsedMessage}} />
     )
 }
